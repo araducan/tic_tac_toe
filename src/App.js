6b7c3fb45f0ffd1935fa611d79bc696f874e4606
Andreea R <araducan0805@gmail.com>
2021-08-02 09:33:29 +0300

    tictactoe

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Game from "./Game";
 
 function App() {
-	return <div className="App">Helloooooo</div>;
+	return (
+		<div className="App">
+			<Game />
+		</div>
+	);
 }
 
 export default App;
